c72b8f9e0603cfc376051929dfb8634cfb6f3e28
Юшин Артем <artemiushin@mail.ru>
2022-12-21 21:16:05 +0300

    Aviary add commit (not dodelano)

--- Animal.py
+++ Animal.py
@@ -1,17 +1,16 @@
 class Animal:
 
-    ANIMAL_PREDATOR = 'Хищник'
-    ANIMAL_HERBIVORE = 'Травоядное'
-
-    def __init__(self, name, volume_of_food, age):
+    def __init__(self, name, volume_of_food, food, age):
         self.name = name
         self._volume_of_food = volume_of_food
         self.age = age
+        self._food = food
         self.sound = ''
         self._animal_type = ''
         self._biome = ''
         self._area = ''
-        self._predator_or_herbivore = bool
+        self._predator = bool
+        self.eat_till_full = bool
 
     def play(self):
         print(self.name, ': Я поиграл')
@@ -20,130 +19,135 @@ class Animal:
         print(self.name, ':', self.sound)
 
     def eat(self):
-        print(self.name, ':', 'Я ем', self.volume_of_food)
+        print(self.name, ':', 'Я ем', self.volume_of_food, self.food)
 
     @property
     def volume_of_food(self):
         return self._volume_of_food
 
     @property
-    def view(self):
+    def animal_type(self):
         return self._animal_type
+
+    @property
+    def food(self):
+        return self._food
 
     @property
     def biome(self):
         return self._biome
 
     @property
-    def square(self):
+    def area(self):
         return self._area
 
     @property
-    def predator_or_herbivore(self):
-        return self._predator_or_herbivore
+    def predator(self):
+        return self._predator
 
-    @property
-    def animal_type(self):
-        return self._animal_type
+    @area.setter
+    def area(self, value):
+        if value >= 0:
+            self._area = value
+        else:
+            print('Так нельзя')
 
 
 class Elefant(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'УУХХЕРРРРРРРРРРРРРРРРРР'
         self._animal_type = 'Слон'
         self._biome = 'Пустыня'
-        self.ANIMAL_HERBIVORE = 'Травоядное'
-        self._square = '15 м**2'
+        self._predator = False
+        self._area = 15
 
 
 class Tiger(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'РРРРРР'
         self._animal_type = 'Тигр'
-        self._biome = 'Тропики'
-        self.ANIMAL_PREDATOR = 'Хищник'
-        self._square = '10 м**2'
+        self._biome = 'Равнина'
+        self._predator = True
+        self._area = 10
 
 
 class Pinguin(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'Хрю'
         self._animal_type = 'Пингвин'
         self._biome = 'Тундра'
-        self.ANIMAL_PREDATOR = 'Хищник'
-        self._square = '5 м**2'
+        self._predator = True
+        self._area = 5
 
 
 class Dog(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'Гав'
         self._animal_type = 'Собака'
         self._biome = 'Тундра'
-        self.ANIMAL_PREDATOR = 'Хищник'
-        self._square = '3 м**2'
+        self._predator = True
+        self._area = 3
 
 
 class Sheep(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'БЕЕЕЕ'
         self._animal_type = 'Овца'
         self._biome = 'Горы'
-        self.ANIMAL_HERBIVORE = 'Травоядное'
-        self._square = '8 м**2'
+        self._predator = False
+        self._area = 8
 
 
 class Wolf(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'РРРРРР'
         self._animal_type = 'Волк'
         self._biome = 'Равнина'
-        self.ANIMAL_PREDATOR = 'Хищник'
-        self._square = '3 м**2'
+        self._predator = True
+        self._area = 3
 
 
 class Giraffe(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
-        self.sound = 'Слова жирафа'
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
+        self.sound = 'ПППФФФФФФФ'
         self._animal_type = 'Жираф'
         self._biome = 'Пустыня'
-        self.ANIMAL_HERBIVORE = 'Травоядное'
-        self._square = '20 м**2'
+        self._predator = False
+        self._area = 20
 
 
 class Lion(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'РРРРРРР'
         self._animal_type = 'Лев'
         self._biome = 'Саванна'
-        self.ANIMAL_PREDATOR = 'Хищник'
-        self._square = '12 м**2'
+        self._predator = True
+        self._area = 12
 
 
 class Goat(Animal):
 
-    def __init__(self, name, volume_of_food, age):
-        super().__init__(name, volume_of_food, age)
+    def __init__(self, name, volume_of_food, food, age):
+        super().__init__(name, volume_of_food, food, age)
         self.sound = 'МЕЕЕЕЕЕЕ'
         self._animal_type = 'Козел'
         self._biome = 'Равнина'
-        self.ANIMAL_HERBIVORE = 'Травоядное'
-        self._square = '10 м**2'
-
-
+        self._predator = False
+        self._area = 10
 
--- Aviary.py
+++ Aviary.py
@@ -1,25 +1,50 @@
 from Animal import *
 
 
-class Aviary:
+class Aviary_1:
     def __init__(self, name, biome, area):
         self.name = name
         self.biome = biome
         self.area = area
         self.animals = []
-        self.predator = False
+        self.predator = bool
         self.predator_type = ''
 
     def add(self, Animal: Animal):
-        if Animal.predator_or_herbivore and len(self.animals) == 0:
+        if len(self.animals) == 0 and Animal.predator:
             self.predator = True
-            self.predator_type = Animal._animal_type
-        if Animal.predator_or_herbivore == self.predator and Animal.biome == self.biome:
-            if Animal.predator_or_herbivore and Animal.view == self.predator_type:
+            self.predator_type = Animal.animal_type
+            print(Animal.animal_type, Animal.name, 'подселился в вольер', self.name)
+        if Animal.predator == self.predator and Animal.biome == self.biome:
+            if Animal.predator and Animal.animal_type == self.predator_type:
                 self.animals.append(Animal)
-            elif Animal.predator_or_herbivore:
-                print('Нельзя подселить', Animal._animal_type)
+            elif Animal.predator:
+                print('Нельзя подселить', Animal.animal_type, Animal.name, 'в вольер', self.name)
             else:
                 self.animals.append(Animal)
         else:
-            print('Нельзя подселить', Animal._animal_type)
+            print('Нельзя подселить', Animal.animal_type, Animal.name, 'в вольер', self.name)
+
+
+class Aviary_2:
+    def __init__(self, name, biome, area):
+        self.name = name
+        self.biome = biome
+        self.area = area
+        self.animals = []
+        self.predator = bool
+        self.predator_type = ''
+
+    def add(self, Animal: Animal):
+        if len(self.animals) == 0 and Animal.predator == False:
+            self.predator = False
+            self.predator_type = Animal.animal_type
+            print(Animal.animal_type, Animal.name, 'подселился в вольер', self.name)
+        if Animal.predator == self.predator and Animal.biome == self.biome:
+            self.animals.append(Animal)
+            if Animal.predator:
+                print('Нельзя подселить', Animal.animal_type, Animal.name, 'в вольер', self.name)
+            else:
+                self.animals.append(Animal)
+        else:
+            print('Нельзя подселить', Animal.animal_type, Animal.name, 'в вольер', self.name)
